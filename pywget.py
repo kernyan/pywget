b4a22ee722b95c4bb5697088a89cd1f3704d8417
kernyan <kernyan@users.noreply.github.com>
2021-09-11 18:50:53 -0400

    minor

--- pywget.py
+++ pywget.py
@@ -8,7 +8,11 @@ url = 'serenityos.org/happy/1st/'
 
 # header from Burp suite
 
-temp = 'GET {} HTTP/1.1\r\nHost: {}\r\nCache-Control: max-age=0\r\nUpgrade-Insecure-Requests: 1\r\nUser-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36\r\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9\r\nAccept-Encoding: gzip, deflate\r\nAccept-Language: en-US,en;q=0.9\r\nConnection: close\r\n\r\n'
+path = 'GET {} HTTP/1.1'
+domain = 'Host: {}'
+cache_c = 'Cache-Control: max-age=0\r\nUpgrade-Insecure-Requests: 1'
+user_a = 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36'
+others = 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9\r\nAccept-Encoding: gzip, deflate\r\nAccept-Language: en-US,en;q=0.9\r\nConnection: close'
 
 class pyreq():
 	def __init__(self):
@@ -16,15 +20,14 @@ class pyreq():
 
 	def geturl(self, url):
 		a = re.search(r'org', url)
-		domain, path = url[:a.end()], url[a.end():]
-		self.req = temp.format(path, domain)
+		d = domain.format(url[:a.end()])
+		p = path.format(url[a.end():])
+		self.req = '\r\n'.join([p, d, cache_c, user_a, others]) + '\r\n\r\n'
 		return self.req
 
 if __name__ == '__main__':
 	req = pyreq()
 	a = req.geturl(url)
-	with open('outh.bin', 'w') as f:
-		f.write(a)
 
 
 
